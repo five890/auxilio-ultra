08459dce3519f6d5d8243d4427cbcf7c2006a014
five890 <161637917+five890@users.noreply.github.com>
2026-06-03 00:19:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
+});
